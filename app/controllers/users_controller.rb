5089b1d84cf53e6501f13be3b1904dafeef239de
fernganesan <ganesan1063@gmail.com>
2014-12-23 12:55:18 +0530

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @newUser = User.new
+  end
+  
+  def create 
+    @newUser = User.new(user_params)
+    if @newUser.save
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to user_url(@newUser)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  test "invalid signup information" do
+    get signup_path
+     assert_no_difference 'User.count' do
+       post users_path, user: { name: "ganesn", email: "foo@invalid",
+                                password: "foo", password_confirmation: "bar" }
+     end
+     assert_template 'users/new'
+  end
+  
+  test "valid signup inforamtion" do
+    get signup_path
+     assert_difference 'User.count', 1 do
+       post_via_redirect users_path, user: { name: "ganesan.arunachalam", email: "foo@bar.com",
+                                password: "foobar", password_confirmation: "foobar" }
+     end
+     assert_template 'users/show'
+     assert_equal flash.count, 1
+     assert !!flash[:success]
+  end
+end
